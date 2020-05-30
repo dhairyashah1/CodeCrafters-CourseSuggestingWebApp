e9d24990c594a7a23752abb6a0c731a526b8f0b6
Tushar Bauskar <tsbauskar12@outlook.com>
2020-05-30 10:34:27 +0530

    SignUp and Login forms added

---
+++ coursespot/models.py
@@ -0,0 +1,13 @@
+from coursespot import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(20), nullable=False)
+    lastname = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True) 
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}')"
---
+++ coursespot/routes.py
@@ -0,0 +1,53 @@
+from flask import render_template, redirect, url_for, flash, request
+from coursespot import app, db, bcrypt
+from coursespot.forms import RegistrationForm, LoginForm
+from coursespot.models import User
+from flask_login import login_user, current_user, logout_user, login_required
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(
+            firstname = form.firstname.data,
+            lastname = form.lastname.data,
+            username = form.username.data,
+            email = form.email.data,
+            password = hashed_pw
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("Account created successfully. You can now login", "success")
+        return redirect(url_for('login'))
+    return render_template('register.html', title="Register", form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            flash("Login successful", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login unsuccessful. Please check your email and password", "danger")
+    return render_template('login.html', title="Login", form=form)  
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+@app.route("/help")
+def help():
+    return render_template("help.html", title="Help")
+
+@app.route("/logout")
+def logout():
+    return redirect(url_for('home'))
