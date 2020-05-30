7f7b55a7d0bf44e7c28904e683c25235925bb7af
Tushar Bauskar <tsbauskar12@outlook.com>
2020-05-30 11:12:33 +0530

    Authentication Added

--- coursespot/models.py
+++ coursespot/models.py
@@ -1,7 +1,11 @@
-from coursespot import db
+from coursespot import db, login_manager
+from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     firstname = db.Column(db.String(20), nullable=False)
     lastname = db.Column(db.String(20), nullable=False)
--- coursespot/routes.py
+++ coursespot/routes.py
@@ -12,6 +12,8 @@ def home():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_pw = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -30,15 +32,19 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email = form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
-            flash("Login successful", "success")
-            return redirect(url_for('home'))
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash("Login unsuccessful. Please check your email and password", "danger")
     return render_template('login.html', title="Login", form=form)  
+
 
 @app.route("/about")
 def about():
@@ -50,4 +56,10 @@ def help():
 
 @app.route("/logout")
 def logout():
+    logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template("account.html", title="Account")
