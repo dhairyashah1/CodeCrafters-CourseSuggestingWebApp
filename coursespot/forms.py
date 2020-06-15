462811b9bde1aa3f7e585380c9abd68a26f875d8
Tushar Bauskar <tusharsb12@outlook.com>
2020-06-15 13:23:25 +0530

    .

--- coursespot/forms.py
+++ coursespot/forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) 
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm  Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
