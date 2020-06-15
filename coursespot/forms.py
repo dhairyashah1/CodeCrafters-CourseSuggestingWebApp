d29b66ad305b1debbf1e0ebe155f2f7cbb33789e
Tushar Bauskar <tusharsb12@outlook.com>
2020-06-15 13:36:03 +0530

    Update form added

--- coursespot/forms.py
+++ coursespot/forms.py
@@ -2,10 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 from coursespot.models import User
-
+from flask_login import current_user
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) 
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)]) 
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm  Password', validators=[DataRequired(), EqualTo('password')])
@@ -33,3 +33,21 @@ class ContactForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()]) 
     message = StringField('Message', validators=[DataRequired(), Length(max=300)])
     submit = SubmitField('Submit')
+
+
+class UpdateForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("That username already exists. Please select a different one.")
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("Email already in use. Please use different one.")
