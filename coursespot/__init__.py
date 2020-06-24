c5b3821d4cdee1383ea853b10de1e63c34a8cff5
utsavk28 <62751545+utsavk28@users.noreply.github.com>
2020-06-24 14:59:02 +0530

    Update __init__.py

--- coursespot/__init__.py
+++ coursespot/__init__.py
@@ -16,8 +16,8 @@ login_manager.login_message_category = 'danger'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'sender@email.com'
-app.config['MAIL_PASSWORD'] = 'Password'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 from coursespot import routes
