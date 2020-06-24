fd7a8c07dd51c8293630f53cb85c79d5532a90b2
utsavk28 <62751545+utsavk28@users.noreply.github.com>
2020-06-24 13:14:19 +0530

    Update models.py

--- coursespot/models.py
+++ coursespot/models.py
@@ -1,5 +1,7 @@
-from coursespot import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from coursespot import db, login_manager,app
 from flask_login import UserMixin
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -7,11 +9,31 @@ def load_user(user_id):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    firstname = db.Column(db.String(20), nullable=False)
-    lastname = db.Column(db.String(20), nullable=False)
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(50), nullable=False, unique=True) 
     password = db.Column(db.String(60), nullable=False)
 
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
     def __repr__(self):
         return f"User('{self.username}','{self.email}')"
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), nullable=False) 
+    message = db.Column(db.String(300), nullable=False)
+
+    def __repr__(self):
+        return f"Contact('{self.email}', '{self.message}')"
