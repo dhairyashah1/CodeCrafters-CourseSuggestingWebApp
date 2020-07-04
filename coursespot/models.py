2d24d121d13ebcb5d07eaf263926357a30e97016
Tushar Bauskar <tsbauskar12@outlook.com>
2020-07-04 23:30:41 +0530

    Removed Contact model

--- coursespot/models.py
+++ coursespot/models.py
@@ -30,10 +30,3 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"User('{self.username}','{self.email}')"
 
-class Contact(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), nullable=False) 
-    message = db.Column(db.String(300), nullable=False)
-
-    def __repr__(self):
-        return f"Contact('{self.email}', '{self.message}')"
